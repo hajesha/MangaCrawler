878858809aab64fe3053a66636e792276ce4627e
Hajesha <hkandasamy@ryerson.ca>
2020-03-09 02:27:37 -0400

    get title from site

---
+++ mangacrawler/mangacrawler/spiders/mangacrawler_spider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+class MangaCrawler(scrapy.Spider):
+    name = "mangacrawler"
+    def start_requests(self):
+
+        url = 'https://mangadex.org/titles'
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for manga in response.css('div.manga-entry'):
+            yield {
+                'title': manga.css('div.text-truncate a.manga_title::text').get(),
+                # 'author': quote.css('small.author::text').get(),
+                # 'tags': quote.css('div.tags a.tag::text').getall(),
+            }
